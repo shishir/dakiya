d0094e58b4d4a1274c1dba43a87030859a9dbb6f
Shishir Das <shishir.das@gmail.com>
2018-09-13 23:32:35 +1000

    Refactor

--- lib/dakiya/mailer.ex
+++ lib/dakiya/mailer.ex
@@ -4,7 +4,7 @@ defmodule Dakiya.Mailer do
     acc = validate_presence_of("body", args, acc, fn(v) -> Map.has_key?(v, "template") end)
     acc = validate_presence_of("subject", args, acc)
     data = if length(acc) == 0 do
-      args
+      {:ok, args}
     else
       {:error,  acc}
     end
@@ -13,6 +13,7 @@ defmodule Dakiya.Mailer do
 
   defp validate_presence_of(field, data, acc, skip_if \\ fn(x) -> false end) do
     acc = if ("" == data[field] or nil == data[field]) and !skip_if.(data) do
+      #TODO: This is slow. use |
       acc ++ [%{field => "cannot be blank"}]
     else
       acc
@@ -21,24 +22,24 @@ defmodule Dakiya.Mailer do
   end
 
   def transform(err = {:error, _}), do: err
-  def transform(data) do
+  def transform({:ok, data}) do
     html = if data["body"] do
             data["body"]
            else
             load_template(data["template"]["name"], data["template"]["message"])
            end
-    %{
+    {:ok, %{
       to: data["to"],
       subject: data["subject"],
       html: html
-    }
+    }}
   end
 
   def merge_defaults(err = {:error, _}), do: err
-  def merge_defaults(data) do
-   Map.merge(%{
+  def merge_defaults({:ok, data}) do
+   {:ok, Map.merge(%{
       from: "shishir.das@gmail.com"
-    }, data)
+    }, data)}
   end
 
   def load_template("password-reset", message) do
